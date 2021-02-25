8f36c02f4a6a69436d1a8dcce86edf639795488c
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-02-25 14:56:31 +0200

    Add ability to choose kitten amount

---
+++ src/features/kitten/components/CustomAmountDialog.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Dialog from 'react-native-dialog';
+
+interface CustomAmountDialogProps {
+  visible: boolean;
+  onClose: () => void;
+  onSet: (amount: number) => void;
+}
+
+export default function CustomAmountDialog({
+  visible,
+  onSet,
+  onClose
+}: CustomAmountDialogProps) {
+  const [amount, setAmount] = useState<number | null>(null);
+
+  const handleSet = () => {
+    if (amount !== null) onSet(amount);
+  };
+
+  const handleChange = (text: string) => {
+    if (text.length === 0) {
+      setAmount(null);
+      return;
+    }
+    const newAmount = Number(text.replace(/[^0-9]/g, ''));
+    setAmount(newAmount);
+  };
+
+  const amountValue = amount !== null ? String(amount) : '';
+
+  return (
+    <View style={styles.container}>
+      <Dialog.Container visible={visible} onBackdropPress={onClose}>
+        <Dialog.Title>Set a custom kitten amount</Dialog.Title>
+        <Dialog.Description>
+          How many kittens would you like to see?
+        </Dialog.Description>
+        <Dialog.Input
+          onChangeText={handleChange}
+          value={amountValue}
+          keyboardType="numeric"
+          placeholder="Kitten amount"
+        />
+        <Dialog.Button label="Cancel" onPress={onClose} />
+        <Dialog.Button label="Set" onPress={handleSet} />
+      </Dialog.Container>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- src/features/kitten/screens/KittenList.tsx
+++ src/features/kitten/screens/KittenList.tsx
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
-import { Dimensions, FlatList, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 import { useAppDispatch, useAppSelector } from 'src/store';
 
+import CustomAmountDialog from '../components/CustomAmountDialog';
 import KittenListing from '../components/KittenListing';
 import { KittenDTO } from '../kittenDto';
 import { fetchKittensByAmount, selectKittens } from '../kittenSlice';
@@ -11,19 +19,69 @@ const { width } = Dimensions.get('window');
 
 export default function KittenListScreen() {
   const kittens = useAppSelector(selectKittens);
+  const status = useAppSelector((state) => state.kitten.status);
   const dispatch = useAppDispatch();
 
+  const [amount, setAmount] = useState(30);
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
-    dispatch(fetchKittensByAmount(3));
-  }, [dispatch]);
+    dispatch(fetchKittensByAmount(amount));
+  }, [dispatch, amount]);
 
   const renderItem = ({ item }: { item: KittenDTO }) => (
     <KittenListing key={item.id} kitten={item} />
   );
 
+  const handleOpen = () => {
+    setVisible(true);
+  };
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  const handleSet = (newAmount: number) => {
+    setAmount(newAmount);
+    setVisible(false);
+  };
+
+  if (status === 'pending') {
+    return (
+      <View style={styles.container}>
+        <Text>Loading kittens :3</Text>
+      </View>
+    );
+  }
+
+  const renderListHeaderComponent = () => {
+    return (
+      <>
+        <View style={styles.buttons}>
+          <View style={[styles.buttonContainer, { marginRight: 8 }]}>
+            <Button title="30" onPress={() => setAmount(30)} />
+          </View>
+          <View style={[styles.buttonContainer, { marginVertical: 8 }]}>
+            <Button title="50" onPress={() => setAmount(50)} />
+          </View>
+          <View style={[styles.buttonContainer, { marginLeft: 8 }]}>
+            <Button title="100" onPress={() => setAmount(100)} />
+          </View>
+        </View>
+        <Button title="Custom amount" onPress={handleOpen} />
+      </>
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <CustomAmountDialog
+        visible={visible}
+        onSet={handleSet}
+        onClose={handleClose}
+      />
       <FlatList
+        ListHeaderComponent={renderListHeaderComponent()}
         directionalLockEnabled={true}
         horizontal={false}
         showsHorizontalScrollIndicator={false}
@@ -40,7 +98,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttons: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8
+  },
+  buttonContainer: {
+    flex: 1
   },
   contentContainer: {
     paddingHorizontal: 16,
